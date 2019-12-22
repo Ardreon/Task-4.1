1e8557b62898860f8e35a4b2a784230fdeabbff5
George Cherkashyn <kretroll@gmail.com>
2019-12-23 01:46:09 +0200

    Task 4.1 . Доделанные тесты

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,6 +1,8 @@
 public class TemperatureConverter {
-    public double convertFtoC(double degreesFahrenheit)
-    { return (degreesFahrenheit - 32.0) / 9.0 * 5.0; }
+
+    public double convertFtoC(double degreesFahrenheit) {
+        return (degreesFahrenheit - 32.0) / 9.0 * 5.0;
+    }
     public double convertCtoF(double degreesCelsius) {
         return degreesCelsius / 5.0 * 9.0 + 32.0;
     }
@@ -10,11 +12,11 @@ public class TemperatureConverter {
         public double convertKtoC(double kelvin) {
         return kelvin - 273.2;
     }
-        public double convertFtoK(double degreesFahrenheit)
-        { return (degreesFahrenheit + 459.7) / 9.0 * 5.0;
+        public double convertFtoK(double degreesFahrenheit) {
+        return (degreesFahrenheit + 459.7) / 9.0 * 5.0;
      }
-        public double convertKtoF(double kelvin)
-        { return kelvin / 5.0 * 9.0 - 459.7;
+        public double convertKtoF(double kelvin) {
+        return kelvin / 5.0 * 9.0 - 459.7;
      }
 }
 
--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -22,13 +22,23 @@ public class TemperatureConverterTest {
        @Parameterized.Parameter(2)
        public double expected1;
 
+       @Parameterized.Parameter(3)
+       public double expected2;
+
+       @Parameterized.Parameter(4)
+       public double expected3;
+
+       @Parameterized.Parameter(5)
+       public double expected4;
+
+
        @Parameterized.Parameters
        public static Collection data(){
            return Arrays.asList(new Object[][] {
-                   {0.0, 273.2, 32.0},
-                   {-273.2, 0.0, -459.4},
-                   {3.0, 276.2, 37.4},
-                   {-5.0, 268.2, 23.0}
+                   {0.0, 273.2, 32.0, -17.7, 255.3, -459.7},
+                   {-273.2, 0.0, -459.4, -169.5, 103.6, -951.46},
+                   {3.0, 276.2, 37.4, -16.1, 257.0, -454.3},
+                   {-5.0, 268.2, 23.0, -20.5, 252.6, -468.7}
            });
        }
 
@@ -46,6 +56,8 @@ public class TemperatureConverterTest {
 
     @Test
     public void convertFtoC() {
+           assertEquals(expected2,converter.convertFtoC(parametr), 0.1);
+
     }
 
     @Test
@@ -60,9 +72,11 @@ public class TemperatureConverterTest {
 
     @Test
     public void convertFtoK() {
+        assertEquals(expected3, converter.convertFtoK(parametr), 0.09);
     }
 
     @Test
     public void convertKtoF() {
+        assertEquals(expected4, converter.convertKtoF(parametr), 0.0001);
     }
 }
